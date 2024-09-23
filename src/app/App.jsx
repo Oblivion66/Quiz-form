7efceff4a68b8bb9e3d563597f531b2fa43f128a
unknown <myradusmanov@gmail.com>
2024-09-24 01:18:17 +0300

    finished

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,73 @@
-import './App.scss'
+import { useState } from "react";
+import "./App.scss";
+import Result from "../components/Result";
+import Game from "../components/Game";
+
+const questions = [
+  {
+    title: "React - это ... ?",
+    variants: ["библиотека", "фреймворк", "приложение"],
+    correct: 0,
+  },
+  {
+    title: "Компонент - это ... ",
+    variants: [
+      "приложение",
+      "часть приложения или страницы",
+      "то, что я не знаю что такое",
+    ],
+    correct: 1,
+  },
+  {
+    title: "Что такое JSX?",
+    variants: [
+      "Это простой HTML",
+      "Это функция",
+      "Это тот же HTML, но с возможностью выполнять JS-код",
+    ],
+    correct: 2,
+  },
+  {
+    title: "Сколько сторон света существует?",
+    variants: [
+      "4",
+      "5",
+      "6",
+    ],
+    correct: 0,
+  },
+  {
+    title: "Как зовут Короля Англии?",
+    variants: ["Вильгельм V", "Карл III", "Генри IV"],
+    correct: 1,
+  },
+];
 
 function App() {
-  
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    setStep(step + 1);
+
+    if (index == question.correct) setCorrect(correct + 1);
+  };
 
   return (
-    <>
-<div>djsjds</div>
-    </>
-  )
+    <div className="App">
+      {step != questions.length ? (
+        <Game
+          step={step}
+          question={question}
+          questions={questions}
+          onClickVariant={onClickVariant}
+        />
+      ) : (
+        <Result correct={correct} questions={questions} />
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
